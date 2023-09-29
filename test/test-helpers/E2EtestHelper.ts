a26be8c72e0f744eee3d6b25bb1b29e1661c3236
yourijanssen <64319462+yourijanssen@users.noreply.github.com>
2023-09-29 15:52:10 +0200

    fixed Sinon dependency issue

--- test/test-helpers/E2EtestHelper.ts
+++ test/test-helpers/E2EtestHelper.ts
@@ -1,21 +1,20 @@
-import Sinon = require('sinon');
 import express from 'express';
 import { HeroController } from '../../src/controller/hero';
 import { HeroMysqlDatabase } from '../../src/data/hero-sql';
 import { HeroService } from '../../src/service/hero';
 
-export function getHeroServiceStub(sandbox: Sinon.SinonSandbox): HeroService {
+export function getHeroServiceStub(sandbox: sinon.SinonSandbox): HeroService {
   const heroDatabaseStub: HeroMysqlDatabase = new HeroMysqlDatabase();
   // Stub the getHeroes method
-  const getHeroesStub: Sinon.SinonStub = sandbox.stub(heroDatabaseStub, 'getHeroes');
+  const getHeroesStub: sinon.SinonStub = sandbox.stub(heroDatabaseStub, 'getHeroes');
   getHeroesStub.returns('[{ id: 1, name: "Hero 1" }]');
   // Stub the getHeroById method
-  const getHeroByIdStub: Sinon.SinonStub = sandbox.stub(heroDatabaseStub, 'getHeroById');
+  const getHeroByIdStub: sinon.SinonStub = sandbox.stub(heroDatabaseStub, 'getHeroById');
   getHeroByIdStub.returns('{ id: 1, name: "Hero 1" }');
   return new HeroService(heroDatabaseStub);
 }
 
-export function getHeroApp(sandbox: Sinon.SinonSandbox): express.Express {
+export function getHeroApp(sandbox: sinon.SinonSandbox): express.Express {
   const app: express.Express = express();
   const heroController: HeroController = new HeroController(getHeroServiceStub(sandbox));
   // Set up /hero/all route
